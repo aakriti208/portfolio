cd3d42f011691ec120ad59b314ee66e51b097398
aakriti208 <kookybairn@gmail.com>
2022-05-20 11:41:46 +0545

    few changes made

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React aakriti
-        </a>
-      </header>
+      <h1 color="grey">HELLO. WELCOME TO MY WORLD OF AWESOMENESS!!!</h1>
     </div>
   );
 }
